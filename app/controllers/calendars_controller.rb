2c5b034931df7098e89fa097e5a0e0e8c1d4b9ae
takared44 <86856476+takared44@users.noreply.github.com>
2021-07-14 08:46:09 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -11,7 +11,7 @@ class CalendarsController < ApplicationController
     Plan.create(plan_params)
     redirect_to action: :index
   end
-#Issue2分かりません
+#Issue2分かりません...
   private
 
   def plan_params
