6cc9b037a0791dc494720464f694cbd84ce20eec
takared44 <86856476+takared44@users.noreply.github.com>
2021-07-18 19:37:47 +0900

    Issue4.5.6

--- app/javascript/articles.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(document).on("mouseover", "h1", function () {
-    $(this).css({ "color": "blue" });
-  }).on("mouseout", "h1", function () {
-    $(this).css({ "color": "" });
-  })
-});
